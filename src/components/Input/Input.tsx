f192ded306e861ef17ba41bcc3412bc30bdc1b04
Askar Pulatov <master15994@gmail.com>
2023-08-24 15:25:59 +0300

    storybook

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const Input = ({ primary, size, label, border }) => {
+    return (
+        <input
+            type="text" />
+    )
+}
+
+
+
+
+export default Input
